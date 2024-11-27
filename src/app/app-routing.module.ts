3e1135d4743aee935258e41518aff3d528dfc893
Jon Meaney <jon.meaney@mercedes-benz.com>
2024-11-26 22:01:26 -0500

    Removing route guards, client-side control has minimal impact on security

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { authGuard } from './core/auth.guard';
-import { roleGuard } from './core/role.guard';
 
 const routes: Routes = [
   // Lazy load Auth Module for login
@@ -14,16 +12,14 @@ const routes: Routes = [
   {
     path: 'user-dashboard',
     loadChildren: () => import('./features/user/user.module').then(m => m.UserModule),
-    canActivate: [authGuard, roleGuard],
-    data: { roles: ['USER'] }, // Restrict access to 'USER' role
+
   },
 
   // Lazy load Admin Dashboard with guards
   {
     path: 'admin-dashboard',
     loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule),
-    canActivate: [authGuard, roleGuard],
-    data: { roles: ['ADMIN'] }, // Restrict access to 'ADMIN' role
+
   },
 
   // Fallback route for undefined paths
